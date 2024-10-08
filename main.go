7ceba5d1343e7479ff506f9b425d0ef5478474ac
Chris Cadden <cadden.cs@gmail.com>
2024-10-08 14:57:55 -0400

    refactor: use state instead of boolean

--- main.go
+++ main.go
@@ -3,26 +3,50 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
+)
+
+var recipeDirectory string = "./recipes"
+
+const (
+	Shopping = iota
+	ViewCart
+	Checkout
 )
 
 type model struct {
 	recipes []string
 	cursor  int
 	cart    map[string]int
-	debug   bool
+	state   int
+}
+
+func getRecipeNames() []string {
+	entries, err := os.ReadDir(recipeDirectory)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	names := make([]string, len(entries))
+
+	for i, e := range entries {
+		names[i] = strings.TrimSuffix(e.Name(), ".json")
+	}
+
+	return names
 }
 
 func initialModel() model {
 	return model{
-		recipes: []string{"a", "b", "c"},
+		recipes: getRecipeNames(),
 		cart:    make(map[string]int),
+		state:   Shopping,
 	}
 }
 
 func (m model) Init() tea.Cmd {
-	// thinking this is where we can read in recipe slugs from some central directory
 	return nil
 }
 
@@ -31,7 +55,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
+		case "ctrl+c":
 			return m, tea.Quit
 		case "up", "k":
 			if m.cursor > 0 {
@@ -41,15 +65,24 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if m.cursor < len(m.recipes)-1 {
 				m.cursor++
 			}
-		case "enter", " ":
+		case "enter", " ", "a":
 			_, ok := m.cart[m.recipes[m.cursor]]
 			if ok {
 				m.cart[m.recipes[m.cursor]]++
 			} else {
 				m.cart[m.recipes[m.cursor]] = 1
 			}
+		case "backspace", "delete", "x":
+			_, ok := m.cart[m.recipes[m.cursor]]
+			if ok {
+				m.cart[m.recipes[m.cursor]]--
+			} else {
+				m.cart[m.recipes[m.cursor]] = 0
+			}
 		case "p":
-			m.debug = true
+			m.state = ViewCart
+		case "s":
+			m.state = Shopping
 		}
 	}
 
@@ -57,45 +90,50 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	if m.debug {
-		defer func() { m.debug = false }()
-		s := fmt.Sprintf("\nDump of cart:\n")
+	s := ""
+	switch m.state {
+	case ViewCart:
+		s = fmt.Sprintf("\nDump of cart:\n")
 
 		for key, val := range m.cart {
 			s += fmt.Sprintf("\n%s: %v\n", key, val)
 		}
 
+		s += "\nPress s to return to browsing.\n"
+
 		return s
-	}
+	case Shopping:
+		// The header
+		s += "What are we eating this week?\n\n"
 
-	// The header
-	s := "What should we buy at the market?\n\n"
+		// Iterate over our choices
+		for i, choice := range m.recipes {
 
-	// Iterate over our choices
-	for i, choice := range m.recipes {
+			// Is the cursor pointing at this choice?
+			cursor := " " // no cursor
+			if m.cursor == i {
+				cursor = ">" // cursor!
+			}
 
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
+			// Render the row
+			s += fmt.Sprintf("%s [%v] %s\n", cursor, m.cart[choice], choice)
 		}
 
-		// Render the row
-		s += fmt.Sprintf("%s %s\n", cursor, choice)
+		// The footer
+		s += "\nPress p to print.\n"
+		s += "\nPress q to quit.\n"
+
+		// Send the UI for rendering
+		return s
+	default:
+		return s
 	}
-
-	// The footer
-	s += "\nPress p to print.\n"
-	s += "\nPress q to quit.\n"
-
-	// Send the UI for rendering
-	return s
 }
 
 func main() {
 	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
+		fmt.Printf("Error: %v", err)
 		os.Exit(1)
 	}
 }
